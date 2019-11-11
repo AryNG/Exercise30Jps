35e4577fcbac096f39d8890feefa334131e1ba15
AryNG <red.codes.lifes@gmail.com>
2019-11-11 09:54:46 -0600

    Jsp practices

---
+++ src/com/exercise30jsp/model/message.java
@@ -0,0 +1,21 @@
+package com.exercise30jsp.model;
+
+public class message {
+	private String texto;
+	
+	public message() 
+	{
+		
+	}
+	public message(String texto)
+	{
+		this.texto=texto;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto=texto;
+	}
+
+}
